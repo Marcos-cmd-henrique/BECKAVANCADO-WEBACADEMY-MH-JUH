1a0bdd3e8fb8972f519a6773b0623767047f101b
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-12-07 19:20:46 -0500

    outra maneira da mesma função async

--- project-favorites/public/scripts.js
+++ project-favorites/public/scripts.js
@@ -15,7 +15,7 @@ const form = document.querySelector('form')
 
 async function load(){
     const res = await fetch('http:localhost:3000').then(data => data.json())
-    res.urls.map(item => addElement(item)) 
+    res.urls.map(({name, url}) => addElement({name, url})) 
 }
 
 load()
